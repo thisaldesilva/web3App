7c3db3bbb6bc42848712ff01db81ee59430769b6
Your Name <you@example.com>
2023-12-06 12:22:59 +0530

    Admin + farmer

--- frontend/src/components/farmer.tsx
+++ frontend/src/components/farmer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 function AdminPage() {
   return (
     <div>
-      <h1>Admin Page</h1>
-      <p>This is a page exclusively for administrators.</p>
+      <h1>Farmer Page</h1>
+      <p>This is a page exclusively for Famer activities.</p>
     </div>
   );
 }
