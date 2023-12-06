8ce4606aeaa1618c7b5d80c09ae1cd08151812dd
Your Name <you@example.com>
2023-12-06 12:21:15 +0530

    farmer as the admin and login page added

---
+++ frontend/src/components/farmer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdminPage() {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <p>This is a page exclusively for administrators.</p>
+    </div>
+  );
+}
+
+export default AdminPage;
---
+++ frontend/src/components/login.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../components/Auth/AuthenticationContext';
+
+function LoginPage() {
+    const navigate = useNavigate();
+    const { login } = useAuth();
+    const [credentials, setCredentials] = useState({ username: '', password: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Credentials:', credentials);
+
+    // Dummy logic for role-based redirection
+    
+    const role = credentials.username === 'farmer' ? 'farmer' : 'backer';
+    login({ ...credentials, role });
+
+    if (role === 'backer') {
+        navigate('/backer');
+      } else {
+        navigate('/farmer');
+      }
+  };
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={credentials.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={credentials.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
