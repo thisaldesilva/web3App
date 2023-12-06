2dcbef6e2f6283ee9ca52aea494dbbfe7eb06965
Your Name <you@example.com>
2023-12-06 11:48:10 +0530

    Protected Routes added

---
+++ frontend/src/components/Routes/ProtectedRoutes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../Auth/AuthenticationContext';
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
