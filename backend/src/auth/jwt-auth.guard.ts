ce8464bed1b22f304f9e04fab131c0be0852c526
thisaldesilva <thisal@live.com>
2023-12-08 18:03:15 +0530

    backend now dont use the Authorization header

--- backend/src/auth/jwt-auth.guard.ts
+++ backend/src/auth/jwt-auth.guard.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -8,4 +9,17 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     }
 
     // You can extend the functionality by overriding handleRequest, canActivate, etc.
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest();
+        console.log("request -> ", request.cookies)
+        const token = request.cookies['jwt'];
+        console.log("JWT ->", token)
+
+    
+        if (token) {
+          request.headers.authorization = `Bearer ${token}`;
+        }
+    
+        return super.canActivate(context);
+      }
 }
